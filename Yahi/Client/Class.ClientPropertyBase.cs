40734371930ef4ca967694b044a547f7fee21e6b
DevBot9 <simon@devbot.lt>
2021-01-11 08:31:59 +0200

    Adding ClientPropertyBase that abstracts all the common stuff for client-side properties.

---
+++ Yahi/Client/Class.ClientPropertyBase.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+
+namespace DevBot.Homie {
+    public class ClientPropertyBase {
+        protected IBroker _broker;
+        protected string _topicPrefix;
+        protected string _propertyId;
+
+        private string _name = "";
+        public string Name {
+            get { return _name; }
+            protected set {
+                _name = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        private DataType _dataType = DataType.String;
+        public DataType DataType {
+            get { return _dataType; }
+            protected set {
+                _dataType = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Format)));
+            }
+        }
+
+        private string _format = "";
+        public string Format {
+            get { return _format; }
+            protected set {
+                _format = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Format)));
+            }
+        }
+
+        private string _unit = "";
+        public string Unit {
+            get { return _unit; }
+            protected set {
+                _unit = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Unit)));
+            }
+        }
+
+        private string _value = "";
+        public string Value {
+            get { return _value; }
+            protected set {
+                _value = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected ClientPropertyBase(string topicPrefix, string propertyId) {
+            _topicPrefix = topicPrefix;
+            _propertyId = propertyId;
+        }
+
+        protected void Initialize(IBroker broker) {
+            _broker = broker;
+
+            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$name", (topic, value) => {
+                Name = value;
+            });
+
+            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$datatype", (topic, value) => {
+                DataType = DataType.String;
+            });
+
+            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$format", (topic, value) => {
+                Format = value;
+            });
+
+            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$unit", (topic, value) => {
+                Unit = value;
+            });
+
+            _broker.Subscribe($"{_topicPrefix}/{_propertyId}", (topic, value) => {
+                Value = value;
+            });
+        }
+    }
+}
--- Yahi/Client/Class.ClientStateProperty.cs
+++ Yahi/Client/Class.ClientStateProperty.cs
@@ -1,51 +1,12 @@
-﻿using System.ComponentModel;
-
-namespace DevBot.Homie {
-    public class ClientStateProperty : INotifyPropertyChanged {
-        IBroker _broker;
-        readonly string _topicPrefix;
-        readonly string _propertyId;
-
-        public string Name { get; private set; }
-        public DataType DataType { get; private set; }
-        public string Format { get; private set; }
-        public string Unit { get; private set; }
-        public string Value { get; private set; }
-
-        public event PropertyChangedEventHandler PropertyChanged = delegate { };
-
-        public ClientStateProperty(string topicPrefix, string propertyId) {
+﻿namespace DevBot.Homie {
+    public class ClientStateProperty : ClientPropertyBase {
+        internal ClientStateProperty(string topicPrefix, string propertyId) : base(topicPrefix, propertyId) {
             _topicPrefix = topicPrefix;
             _propertyId = propertyId;
         }
 
-        internal void Initialize(IBroker broker) {
-            _broker = broker;
-
-            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$name", (topic, value) => {
-                Name = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Name)));
-            });
-
-            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$datatype", (topic, value) => {
-                DataType = DataType.String;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(DataType)));
-            });
-
-            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$format", (topic, value) => {
-                Format = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Format)));
-            });
-
-            _broker.Subscribe($"{_topicPrefix}/{_propertyId}/$unit", (topic, value) => {
-                Unit = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Unit)));
-            });
-
-            _broker.Subscribe($"{_topicPrefix}/{_propertyId}", (topic, value) => {
-                Value = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Value)));
-            });
+        internal new void Initialize(IBroker broker) {
+            base.Initialize(broker);
         }
     }
 }
