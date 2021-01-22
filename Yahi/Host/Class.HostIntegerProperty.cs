d7ee80112eedda11a0d2cc385e086998bffe72ce
DevBot9 <simon@devbot.lt>
2021-01-22 13:10:47 +0200

    Adding HostIntegerProperty.

---
+++ Yahi/Host/Class.HostIntegerProperty.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DevBot9.Protocols.Homie {
+    public class HostIntegerProperty : HostPropertyBase {
+        public PropertyType Type = PropertyType.State;
+
+        public new int Value {
+            get {
+                int returnValue;
+
+                returnValue = int.Parse(_rawValue);
+
+                return returnValue;
+            }
+            set {
+                SetValue(value);
+            }
+        }
+
+        internal HostIntegerProperty(PropertyType protertyType, string propertyId, string friendlyName, DataType dataType, string format, string unit) : base(propertyId, friendlyName, dataType, format, false, true, unit) {
+            Type = protertyType;
+        }
+
+        internal override void Initialize(Device parentDevice) {
+            base.Initialize(parentDevice);
+
+            if (Type == PropertyType.Parameter) {
+                _parentDevice.InternalPropertySubscribe($"{_propertyId}/set", (payload) => {
+                    if (ValidatePayload(payload) == true) {
+                        _rawValue = payload;
+
+                        RaisePropertyChanged(this, new PropertyChangedEventArgs(nameof(Value)));
+
+                        _parentDevice.InternalPropertyPublish($"{_propertyId}", Value.ToString());
+                    }
+                });
+            }
+
+            if (Type == PropertyType.Command) {
+                _parentDevice.InternalPropertySubscribe($"{_propertyId}", (payload) => {
+                    if (ValidatePayload(payload) == true) {
+                        _rawValue = payload;
+
+                        RaisePropertyChanged(this, new PropertyChangedEventArgs(nameof(Value)));
+                    }
+                });
+            }
+        }
+
+        private bool ValidatePayload(string payloadToValidate) {
+            var returnValue = false;
+
+            returnValue = int.TryParse(payloadToValidate, out var _);
+
+            return returnValue;
+        }
+
+        private void SetValue(int valueToSet) {
+            switch (Type) {
+                case PropertyType.State:
+                case PropertyType.Parameter:
+
+                    _rawValue = valueToSet.ToString();
+
+                    _parentDevice.InternalPropertyPublish($"{_propertyId}", _rawValue);
+                    break;
+
+                case PropertyType.Command:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
