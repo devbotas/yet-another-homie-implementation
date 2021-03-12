37329c06b4c55c2c29bfc95c78690b41fd928288
devbotas <simon@devbot.lt>
2021-03-12 09:51:50 +0200

    Cleaning up AirConditionerProducer sample.

--- TestApp/AirConditionerProducer.cs
+++ TestApp/AirConditionerProducer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DevBot9.Protocols.Homie;
 using uPLibrary.Networking.M2Mqtt;
@@ -7,7 +8,6 @@ using uPLibrary.Networking.M2Mqtt;
 namespace TestApp {
     internal class AirConditionerProducer {
         private MqttClient _mqttClient;
-        private string _mqttBrokerIp = "172.16.0.2";
         private string _mqttClientGuid = Guid.NewGuid().ToString();
 
         private HostDevice _hostDevice;
@@ -15,38 +15,40 @@ namespace TestApp {
         private HostFloatProperty _actualAirTemperature;
         private HostEnumProperty _onOffSwitch;
         private HostEnumProperty _actualState;
-        private HostEnumProperty _onOffSwitch2;
         private HostIntegerProperty _ventilationLevel;
         private HostDateTimeProperty _previousServiceDate;
         private HostDateTimeProperty _nextServiceDate;
         private HostBooleanProperty _performServiceCommand;
 
+        public AirConditionerProducer() { }
 
-        private double _simulatedAirTemperature = 20;
-        private double _simulatedTransientTargetTemperature = 21;
-
-        public AirConditionerProducer() {
-            _mqttClient = new MqttClient(_mqttBrokerIp);
+        public void Initialize(string mqttBrokerIpAddress) {
+            _mqttClient = new MqttClient(mqttBrokerIpAddress);
             _mqttClient.MqttMsgPublishReceived += HandlePublishReceived;
-        }
-
-        public void Initialize() {
             _mqttClient.Connect(_mqttClientGuid);
 
             _hostDevice = DeviceFactory.CreateHostDevice("air-conditioner", "Air conditioning unit");
 
-            _hostDevice.UpdateNodeInfo("general", "General information and properties", "no-type");
-            _hostDevice.UpdateNodeInfo("ventilation", "Ventilation information and properties", "no-type");
-            _hostDevice.UpdateNodeInfo("service", "Service ralated properties", "no-type");
+            #region General node
 
+            _hostDevice.UpdateNodeInfo("general", "General information and properties", "no-type");
+
+            // Temperatures. These are pretty self-explanatory, right?
             _actualAirTemperature = _hostDevice.CreateHostFloatProperty(PropertyType.State, "general", "actual-air-temperature", "Actual measured air temperature", "°C");
             _targetAirTemperature = _hostDevice.CreateHostFloatProperty(PropertyType.Parameter, "general", "target-air-temperature", "Target air temperature", "°C");
+
+            // Besides obvious ON and OFF states, there's a transient STARTING state. This simulated the non-instant startup of the device.
+            _actualState = _hostDevice.CreateHostEnumProperty(PropertyType.State, "general", "actual-state", "Actual power state", new[] { "ON", "OFF", "STARTING" });
+
+            // Creating a switch. It also simulates startup sequence ON -> STARTING -> OFF. Shutdown sequence is instant ON -> OFF.
             _onOffSwitch = _hostDevice.CreateHostEnumProperty(PropertyType.Command, "general", "turn-on-off", "Turn device on or off", new[] { "ON", "OFF" });
             _onOffSwitch.PropertyChanged += (sender, e) => {
                 if ((_actualState.Value == "ON") && (_onOffSwitch.Value == "OFF")) {
+                    // This is the shutdown sequence.
                     _actualState.Value = "OFF";
                 }
                 if ((_actualState.Value == "OFF") && (_onOffSwitch.Value == "ON")) {
+                    // This is the startup sequence.
                     _actualState.Value = "STARTING";
                     Task.Run(async () => {
                         await Task.Delay(3000);
@@ -54,11 +56,32 @@ namespace TestApp {
                     });
                 }
             };
-            _actualState = _hostDevice.CreateHostEnumProperty(PropertyType.State, "general", "actual-state", "Actual power state", new[] { "ON", "OFF", "STARTING" });
-            _onOffSwitch2 = _hostDevice.CreateHostEnumProperty(PropertyType.Parameter, "general", "turn-on-off-2", "Turn device on or off 2", new[] { "ON", "OFF" });
+
+
+            #endregion
+
+
+            #region Ventilation node
+
+            _hostDevice.UpdateNodeInfo("ventilation", "Ventilation information and properties", "no-type");
+
+            // Allows user to set ventilation level (or power). This will actually be used in simulation loop.
             _ventilationLevel = _hostDevice.CreateHostIntegerProperty(PropertyType.Parameter, "ventilation", "level", "Level of ventilation", "%");
+
+            #endregion
+
+
+            #region Service node
+
+            _hostDevice.UpdateNodeInfo("service", "Service related properties", "no-type");
+
+            // Previous and next service dates. These are read-only properties. They are changed by "perform service" command.
             _previousServiceDate = _hostDevice.CreateHostDateTimeProperty(PropertyType.State, "service", "previous-service-date", "Date of the last service");
             _nextServiceDate = _hostDevice.CreateHostDateTimeProperty(PropertyType.State, "service", "next-service-date", "Date for the next service");
+
+            // This is a write-only property, that is — a command. It sets last service date to actual datetime, and also sets next service date to some time in the future.
+            // Popular automation software have lots of problems with this kind of workflow, when there's a command that doesn't really have a state register.
+            // As of 2021-03-12, openHAB and HomeAssistant and HoDD can't really deal with it. Which is a bummer, as it a pretty common worklfow in real time! 
             _performServiceCommand = _hostDevice.CreateHostBooleanProperty(PropertyType.Command, "service", "perform-service", "Perform service");
             _performServiceCommand.PropertyChanged += (sender, e) => {
                 if (_performServiceCommand.Value == true) {
@@ -67,6 +90,9 @@ namespace TestApp {
                 }
             };
 
+            #endregion
+
+            // This builds topic trees and subscribes to everything.
             _hostDevice.Initialize((topic, value, qosLevel, isRetained) => {
                 _mqttClient.Publish(topic, Encoding.UTF8.GetBytes(value), 1, true);
 
@@ -74,45 +100,45 @@ namespace TestApp {
                 _mqttClient.Subscribe(new string[] { topic }, new byte[] { 1 });
             });
 
+            // TODO: Values cannot be set before host.Initialize() method is called.
             _actualState.Value = "OFF";
             _targetAirTemperature.Value = 23;
             _ventilationLevel.Value = 50;
             _nextServiceDate.Value = DateTime.Now.AddMonths(3);
 
-            Task.Run(async () => {
-                var localTargetTemperature = 0.0f;
-
-                while (true) {
-                    _simulatedAirTemperature = _simulatedTransientTargetTemperature + 0.3 * Math.Sin(DateTime.Now.Second);
-
-                    if (_actualState.Value == "ON") {
-                        localTargetTemperature = _targetAirTemperature.Value;
-                    }
-                    else {
-                        localTargetTemperature = 25;
-                    }
-
-                    if (localTargetTemperature - _simulatedTransientTargetTemperature > 0.1) {
-                        _simulatedTransientTargetTemperature += 0.1 * _ventilationLevel.Value / 100;
-                    }
-                    if (localTargetTemperature - _simulatedTransientTargetTemperature < -0.1) {
-                        _simulatedTransientTargetTemperature -= 0.1 * _ventilationLevel.Value / 100;
-                    }
-
-                    await Task.Delay(1000);
-
-                    _actualAirTemperature.Value = (float)_simulatedAirTemperature;
-                }
-            });
-
-            Console.WriteLine("Full list of producer's published topics:");
-            foreach (var topic in _hostDevice.GetAllPublishedTopics()) {
-                Console.WriteLine(topic);
-            }
+            // finally, running the simulation loop. We're good to go!
+            Task.Run(async () => await RunSimulationLoopContinuously(new CancellationToken()));
         }
 
         private void HandlePublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e) {
             _hostDevice.HandlePublishReceived(e.Topic, Encoding.UTF8.GetString(e.Message));
+        }
+
+        private async Task RunSimulationLoopContinuously(CancellationToken cancellationToken) {
+            var _simulatedTransientTargetTemperature = 21.0;
+
+            while (cancellationToken.IsCancellationRequested == false) {
+                var _simulatedAirTemperature = _simulatedTransientTargetTemperature + 0.3 * Math.Sin(DateTime.Now.Second);
+
+                float localTargetTemperature;
+                if (_actualState.Value == "ON") {
+                    localTargetTemperature = _targetAirTemperature.Value;
+                }
+                else {
+                    localTargetTemperature = 25;
+                }
+
+                if (localTargetTemperature - _simulatedTransientTargetTemperature > 0.1) {
+                    _simulatedTransientTargetTemperature += 0.1 * _ventilationLevel.Value / 100;
+                }
+                if (localTargetTemperature - _simulatedTransientTargetTemperature < -0.1) {
+                    _simulatedTransientTargetTemperature -= 0.1 * _ventilationLevel.Value / 100;
+                }
+
+                await Task.Delay(1000);
+
+                _actualAirTemperature.Value = (float)_simulatedAirTemperature;
+            }
         }
     }
 }
