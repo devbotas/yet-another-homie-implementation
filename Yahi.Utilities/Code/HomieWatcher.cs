24e20680a050095e416f6b055009320e9599c2c5
devbotas <simon@devbot.lt>
2022-10-07 16:03:32 +0300

    Adding HomieWatcher and PropertySwitcher.

---
+++ Yahi.Utilities/Code/HomieWatcher.cs
@@ -0,0 +1,163 @@
+﻿namespace DevBot9.Protocols.Homie.Utilities;
+
+public record class DeviceUpdatedMessage(string DeviceId, HomieState NewState);
+
+public class HomieWatcher {
+    private readonly ChannelConnectionOptions _channelConnectionOptions = new();
+    private readonly ConcurrentDictionary<string, ClientDevice> _devices = new();
+    private readonly DevBot9.Protocols.Homie.Utilities.YahiTevuxClientConnection _yahiClient = new();
+    private readonly Dictionary<string, string> _responses = new();
+    private bool _isConnecting;
+    private DateTime _timeOfLastUniqueTopic = DateTime.Now;
+    private HomieWatcher() {
+        _yahiClient.PublishReceived += HandleClientConnectionPublishReceived;
+    }
+
+    public static HomieWatcher Instance { get; } = new HomieWatcher();
+    public bool IsConnected => _yahiClient.IsConnected;
+    public Messenger Messenger { get; } = new();
+
+    public bool TryGetClientProperty(string deviceId, string nodeId, string propertyId, out ClientPropertyBase property, out HomieState state) {
+        var propertyFound = false;
+        property = null!;
+        state = HomieState.Lost;
+
+        if (TryGetClientDevice(deviceId, out var clientDevice)) {
+            var myNode = clientDevice.Nodes.FirstOrDefault(n => n.NodeId == nodeId);
+            if (myNode != null) {
+                var myProperty = myNode.Properties.FirstOrDefault(p => p.PropertyId == nodeId + "/" + propertyId);
+                if (myProperty != null) {
+                    property = myProperty;
+                    state = clientDevice.State;
+                    propertyFound = true;
+                }
+            }
+        }
+
+        return propertyFound;
+    }
+    public void Connect(string ipAddress) {
+        _isConnecting = true;
+
+        _channelConnectionOptions.SetHostname(ipAddress);
+
+        DeviceFactory.Initialize("homie");
+
+        // Consumers can share a single connection to the broker.
+        _yahiClient.Initialize(_channelConnectionOptions);
+        _yahiClient.Connect();
+        _yahiClient.SubscribeAndWait("homie/#", QosLevel.AtLeastOnce);
+
+        while ((DateTime.Now - _timeOfLastUniqueTopic).TotalMilliseconds < 500) {
+            Thread.Sleep(100);
+        }
+
+        var topicDump = _responses.Select(r => r.Key + ":" + r.Value).ToArray();
+        var homieTree = HomieTopicTreeParser.Parse(topicDump, DeviceFactory.BaseTopic, out var _, out var _);
+
+        foreach (var device in homieTree) {
+            RebuildDevice(device.Id);
+        }
+
+        _isConnecting = false;
+    }
+
+    public void Disconnect() {
+        _yahiClient.DisconnectAndWait();
+
+        foreach (var device in _devices) {
+            Messenger.Send(new DeviceUpdatedMessage(device.Key, device.Value.State));
+        }
+    }
+
+    public bool TryGetClientDevice(string deviceId, out ClientDevice clientDevice) {
+        var deviceFound = false;
+
+        if (_devices.ContainsKey(deviceId)) {
+            clientDevice = _devices[deviceId];
+            deviceFound = true;
+        }
+        else {
+            clientDevice = null!;
+        }
+
+        return deviceFound;
+    }
+    private void HandleClientConnectionPublishReceived(object sender, DevBot9.Protocols.Homie.PublishReceivedEventArgs e) {
+        var payload = e.Payload;
+
+        var topicShallBeAdded = (_responses.ContainsKey(e.Topic) == false) && (payload != "");
+        var topicShallBeRemoved = _responses.ContainsKey(e.Topic) && (payload == "");
+        var topicShallBeUpdated = _responses.ContainsKey(e.Topic) && (payload != "");
+        if (topicShallBeAdded) {
+            _responses.Add(e.Topic, payload);
+            _timeOfLastUniqueTopic = DateTime.Now;
+        }
+        if (topicShallBeUpdated) {
+            _responses[e.Topic] = payload;
+        }
+        if (topicShallBeRemoved) {
+            _responses.Remove(e.Topic);
+        }
+
+        if ((_isConnecting == false) && (e.Topic[^6..] == "$homie")) {
+            var newDeviceId = e.Topic.Split('/')[1];
+            RebuildDevice(newDeviceId);
+        }
+
+        if ((_isConnecting == false) && (e.Topic[^6..] == "$state")) {
+            var newDeviceId = e.Topic.Split('/')[1];
+            Helpers.TryParseHomieState(e.Payload, out var newState);
+            Messenger.Send(new DeviceUpdatedMessage(newDeviceId, newState));
+        }
+    }
+
+    private void HandleDevicePropertyChangedEvent(object? sender, PropertyChangedEventArgs e) {
+        if (sender is ClientDevice device) {
+            if (e.PropertyName == nameof(device.State)) {
+                Messenger.Send(new DeviceUpdatedMessage(device.DeviceId, device.State));
+            }
+        }
+    }
+
+    private void RebuildDevice(string deviceId) {
+        var deviceDump = _responses.Where(r => r.Key.StartsWith(DeviceFactory.BaseTopic + "/" + deviceId)).Select(r => r.Key + ":" + r.Value).ToArray();
+        var newDeviceTree = HomieTopicTreeParser.Parse(deviceDump, DeviceFactory.BaseTopic, out var _, out var _);
+
+        var deviceShallBeRemoved = false;
+        var deviceShallBeAdded = false;
+        var deviceShallBeReplaced = false;
+
+        if (_devices.ContainsKey(deviceId) && (newDeviceTree.Length == 0)) {
+            deviceShallBeRemoved = true;
+        }
+
+        if ((_devices.ContainsKey(deviceId) == false) && (newDeviceTree.Length == 1)) {
+            deviceShallBeAdded = true;
+        }
+
+        if (_devices.ContainsKey(deviceId) && (newDeviceTree.Length == 1)) {
+            var oldDevice = _devices[deviceId];
+            if (Helpers.TryParseHomieState(newDeviceTree[0].StateAttribute, out var newState)) {
+                if (oldDevice.State != newState) {
+                    deviceShallBeReplaced = true;
+                }
+            }
+        }
+
+        if (deviceShallBeRemoved || deviceShallBeReplaced) {
+            _devices.Remove(deviceId, out var removedDevice);
+            removedDevice!.PropertyChanged -= HandleDevicePropertyChangedEvent;
+            removedDevice.Dispose();
+            Messenger.Send(new DeviceUpdatedMessage(removedDevice.DeviceId, removedDevice.State));
+        }
+
+        if (deviceShallBeAdded || deviceShallBeReplaced) {
+            var newDevice = DeviceFactory.CreateClientDevice(newDeviceTree[0]);
+            _devices[deviceId] = newDevice;
+            newDevice.Initialize(_yahiClient);
+            newDevice.PropertyChanged += HandleDevicePropertyChangedEvent;
+            Messenger.Send(new DeviceUpdatedMessage(newDevice.DeviceId, newDevice.State));
+        }
+    }
+}
---
+++ Yahi.Utilities/Code/PropertySwitcher.cs
@@ -0,0 +1,109 @@
+﻿namespace DevBot9.Protocols.Homie.Utilities;
+
+public partial class PropertySwitcher : INotifyPropertyChanged, IDisposable {
+    public ClientPropertyBase? HomieProperty;
+    private readonly Action _updateValueAction;
+    private string _deviceId = "";
+    private bool _isDisposed = false;
+    private bool _isPropertyFound = false;
+    private string _nodeId = "";
+    private string _propertyId = "";
+    private HomieState _state = HomieState.Init;
+
+    public PropertySwitcher(Action updateValueAction) {
+        _updateValueAction = updateValueAction;
+
+        HomieWatcher.Instance.Messenger.Register<DeviceUpdatedMessage>(this, HandleDeviceUpdatedMessage);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+    public bool IsPropertyFound {
+        get { return _isPropertyFound; }
+        set {
+            if (_isPropertyFound != value) {
+                _isPropertyFound = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(IsPropertyFound)));
+            }
+        }
+    }
+    public HomieState State {
+        get { return _state; }
+        set {
+            if (_state != value) {
+                _state = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(State)));
+            }
+        }
+    }
+
+    public void Dispose() {
+        // A good article explaining how to implement Dispose. https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public void UpdateHomiePropertyMetadata(string deviceId, string nodeId, string propertyId, out string errorMessage) {
+        _deviceId = deviceId;
+        _nodeId = nodeId;
+        _propertyId = propertyId;
+
+        if (HomieWatcher.Instance.TryGetClientProperty(_deviceId, _nodeId, _propertyId, out var property, out var state)) {
+            SwitchHomieProperties(property);
+            errorMessage = "";
+            State = state;
+            IsPropertyFound = true;
+        }
+        else {
+            errorMessage = "Cannot find Homie entity with such parameters.";
+            IsPropertyFound = false;
+        }
+    }
+
+    protected virtual void Dispose(bool isCalledManually) {
+        if (_isDisposed == false) {
+            if (isCalledManually) {
+                if (HomieProperty != null) {
+                    HomieProperty.PropertyChanged -= HandleHomieValueChanged;
+                }
+            }
+
+            // Free unmanaged resources here and set large fields to null.
+
+            _isDisposed = true;
+        }
+    }
+
+    protected virtual void HandleHomieValueChanged(object? sender, PropertyChangedEventArgs e) {
+        _updateValueAction();
+    }
+
+    protected void SwitchHomieProperties(ClientPropertyBase newProperty) {
+        if (HomieProperty != null) {
+            HomieProperty.PropertyChanged -= HandleHomieValueChanged;
+        }
+
+        newProperty.PropertyChanged += HandleHomieValueChanged;
+        HomieProperty = newProperty;
+
+        _updateValueAction();
+    }
+
+    private void HandleDeviceUpdatedMessage(DeviceUpdatedMessage deviceUpdatedMessage) {
+        // Dispatcher.Invoke(() => {
+        if (deviceUpdatedMessage.DeviceId == _deviceId) {
+            if (HomieWatcher.Instance.IsConnected) {
+                if (deviceUpdatedMessage.NewState == HomieState.Ready) {
+                    UpdateHomiePropertyMetadata(_deviceId, _nodeId, _propertyId, out var _);
+                }
+                else {
+                    State = deviceUpdatedMessage.NewState;
+                }
+            }
+            else {
+                IsPropertyFound = false;
+            }
+        };
+        // });
+    }
+}
